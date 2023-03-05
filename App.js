334449dd6892e30e008b4e3a3781e408a89dd824
Adam Bailey <ajbailey564@gmail.com>
2023-03-05 14:20:26 -0600

    expo tutorial

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage = require('./assets/tesla.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+          <Text style={styles.header}>Tesla</Text>
+          <View style={styles.imageContainger}>
+              <ImageViewer placeholderImageSource={PlaceholderImage} />
+          </View>
+          <View style={styles.footerContainer}>
+              <Button theme="primary" label="Choose a photo" />
+              <Button label="Use this photo" />
+          </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    header: {
+        color: '#fff',
+        marginBottom: 5,
+        fontWeight: 'bold',
+        fontSize: 24,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
