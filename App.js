e67bcd2cceb8d5f3f224ff74ce42bec2902c3e62
Adam Bailey <ajbailey564@gmail.com>
2023-03-05 18:47:32 -0600

    init bottom sheet.

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { Card } from 'react-native-paper';
 
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+
 import ImageViewer from './components/ImageViewer';
-import Button from './components/Button';
+//import Button from './components/Button';
 import Header from './components/HeaderBar';
 import HabitCard from './components/HabitCard';
 
 const PlaceholderImage = require('./assets/tesla.jpg');
+
+const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'white',
+        padding: 16,
+        height: 450,
+      }}
+    >
+      <Text>Swipe down to close</Text>
+    </View>
+);
+
+const sheetRef = React.createRef();
+
 
 export default function App() {
   return (
@@ -16,7 +35,7 @@ export default function App() {
       <Header />
         <View style={styles.container}>
             <HabitCard
-                cardTitle="Programming"
+                cardTitle="Programming2"
                 cardSubTitle="5hrs per Week"
             />
             <HabitCard
@@ -31,13 +50,28 @@ export default function App() {
                 cardTitle="Biking"
                 cardSubTitle="5hrs per Week"
             />
+            <Button
+                style={styles.button}
+                title="Open Bottom Sheet"
+                onPress={() => sheetRef.current.snapTo(0)}
+            />
             <StatusBar style="auto" />
         </View>
+      <BottomSheet
+            ref={sheetRef}
+            snapPoints={[450, 300, 0]}
+            borderRadius={10}
+            renderContent={renderContent}
+      />
       </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#212121',
+        color: '#fff',
+    },
     header: {
         color: '#fff',
         marginBottom: 5,
