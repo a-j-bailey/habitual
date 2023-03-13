5cc6767cd2b46b3adb8bdac38f64e84fef287336
Adam Bailey <ajbailey564@gmail.com>
2023-03-13 00:55:39 -0500

    add habit history table

--- App.js
+++ App.js
@@ -17,7 +17,26 @@ function openDatabase() {
     
     db.transaction((tx) => {
         tx.executeSql(
-            'CREATE TABLE IF NOT EXISTS habits (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, hours INT, minutes INT, frequency TEXT)'
+            `CREATE TABLE IF NOT EXISTS habits (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                hours INT,
+                minutes INT,
+                frequency TEXT
+            )`
+        );
+    });
+    
+    db.transaction((tx) => {
+        tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS habit_history (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                habit_id INT,
+                date TEXT,
+                time_started TEXT,
+                time_ended TEXT,
+                total_time TEXT
+            )`
         );
     });
     return db;
