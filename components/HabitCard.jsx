d17a8e3e3a49c9c55a46a25cc1296052f4f32822
Adam Bailey <ajbailey564@gmail.com>
2023-03-13 00:56:55 -0500

    make cards responsive and sorta working timer

--- components/HabitCard.jsx
+++ components/HabitCard.jsx
@@ -1,31 +1,64 @@
+import * as React from 'react';
 import { Avatar, Button, Card, Text, IconButton, ProgressBar } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-const HabitCard = ({ onPress, habit }) => (
-    <Card
-        style={styles.habitCard}
-        elevation={0}
-        onPress={() => onPress(
-            {habit: habit}
-        )}
-    >
-        <Card.Title
-            title={habit.title}
-            titleVariant="titleLarge"
-            subtitle={habit.hours + 'hrs ' + habit.minutes + 'min per week.'}
-            right={(props) => <IconButton {...props} icon="clock" onPress={() => {}} />}
-        />
-        <Card.Content>
-            <ProgressBar progress={0.5} />
-        </Card.Content>
-    </Card>
-);
+import * as HabitHelper from '../utilities/HabitHelper';
+
+function HabitCard({ onPress, habit }) {
+    const [toggleIsRunning, isRunningVal] = HabitHelper.toggleIsRunning();
+    
+    const sheetContent = () => (
+        <View>
+            {(isRunningVal) ? (
+                "00:00"
+            ) : (
+                habit.hours + 'hrs ' + habit.minutes + 'min per week.'
+            )}
+        </View>
+    )
+    
+    return (
+        <Card
+            style={(isRunningVal) ? ( styles.habitRunning ) : ( styles.habitCard )}
+            elevation={0}
+            onPress={() => onPress(
+                {habit: habit}
+            )}
+        >
+            <Card.Title
+                title={habit.title}
+                titleVariant="titleLarge"
+                subtitle={(isRunningVal) ? (
+                        "00:00"
+                    ) : (
+                        habit.hours + 'hrs ' + habit.minutes + 'min per week.'
+                    )
+                }
+                // right={(props) => <IconButton icon={(isRunningVal) ? ( "pause" ) : ( "clock" )} onPress={toggleIsRunning} />}
+            />
+            <Card.Content>
+                <ProgressBar progress={0.5} />
+            </Card.Content>
+        </Card>
+    );
+    
+}
 
 const styles = StyleSheet.create({
     habitCard: {
         marginBottom: 20,
         backgroundColor: '#fff',
+    },
+    habitRunning: {
+        marginBottom: 20,
+        backgroundColor: '#fff',
+        borderWidth: '1px',
+        borderColor: 'green',
+    },
+    timer: {
+        display: 'flex',
+        flexDirection: 'row',
     }
 });
 
--- components/HabitView.jsx
+++ components/HabitView.jsx
@@ -1,52 +1,77 @@
+import * as React from 'react';
 import { StyleSheet, View, Alert } from 'react-native';
 import { Text, Divider, IconButton, ProgressBar, Button } from 'react-native-paper';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-const HabitView = ({sheetRef, habit, onDelete}) => (
-    <View style={styles.outerContainer}>
-        <View>
-            <View style={styles.headerContainer}>
-                <View>
-                    <Text variant="headlineLarge">{habit.title}</Text>
-                    <Text variant="titleMedium">{habit.hours + 'hrs ' + habit.minutes + 'min / ' + habit.frequency}</Text>
+import * as HabitHelper from '../utilities/HabitHelper';
+
+function HabitView({sheetRef, habit, onDelete}) {
+    const [toggleIsRunning, isRunningVal] = HabitHelper.toggleIsRunning();
+    const [seconds, setSeconds] = React.useState(0);
+
+    React.useEffect(() => {
+        if (isRunningVal) {
+            const ticker = setInterval(() => {
+                setSeconds(seconds + 1);
+            }, 1000);
+
+            return () => clearInterval(ticker);
+        }
+    });
+
+    return (
+        <View style={styles.outerContainer}>
+            <View>
+                <View style={styles.headerContainer}>
+                    <View>
+                        <Text variant="headlineLarge">{habit.title}</Text>
+                        {
+                            (isRunningVal) ? (
+                                <Text variant="titleMedium">{HabitHelper.secondsToTime(seconds)}</Text>
+                            ) : (
+                                <Text variant="titleMedium">{habit.hours + 'hrs ' + habit.minutes + 'min / ' + habit.frequency}</Text>
+                            )
+                        }
+                    </View>
+                    <View>
+                        <IconButton icon={(isRunningVal) ? ( "pause" ) : ( "clock" )} onPress={toggleIsRunning} />
+                    </View>
                 </View>
-                <View>
-                    <IconButton icon="clock" onPress={() => {}} />
-                </View>
+                <ProgressBar progress={0.5} />
+                <Text style={{textAlign: 'right'}}>0:00 of {habit.hours}:{habit.minutes}</Text>
+                <Text>{JSON.stringify(habit)}</Text>
             </View>
-            <ProgressBar progress={0.5} />
-            <Text style={{textAlign: 'right'}}>0:00 of {habit.hours}:{habit.minutes}</Text>
+            <Button
+                textColor={'#ff8080'}
+                onPress={() => {
+                    Alert.alert(
+                        'Wow! Hold up',
+                        'Are you sure you want to delete ' + habit.title,
+                        [
+                            {
+                                text: 'Yes, Delete',
+                                onPress: () => onDelete({id: habit.id}),
+                            },
+                            {
+                                text: 'Cancel',
+                                style: 'cancel',
+                            },
+                        ],
+                        {
+                            cancelable: true,
+                            onDismiss: () => Alert.alert(
+                                'This alert was dismissed by tapping outside of the alert dialog.',
+                            ),
+                        },
+                    );
+                }}
+            >
+                Delete Habit
+            </Button>
         </View>
-        <Button
-            textColor={'#ff8080'}
-            onPress={() => {
-                Alert.alert(
-                    'Wow! Hold up',
-                    'Are you sure you want to delete ' + habit.title,
-                    [
-                        {
-                            text: 'Yes, Delete',
-                            onPress: () => onDelete({id: habit.id}),
-                        },
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                        },
-                    ],
-                    {
-                        cancelable: true,
-                        onDismiss: () => Alert.alert(
-                            'This alert was dismissed by tapping outside of the alert dialog.',
-                        ),
-                    },
-                );
-            }}
-        >
-            Delete Habit
-        </Button>
-    </View>
-    
-);
+
+    );
+}
 
 export default HabitView;
 
